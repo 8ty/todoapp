a1c75e87f13c0794c06437809054497a87287d85
test <admin@test.com>
2025-04-11 17:11:46 +0800

    ci(jest): 更新 Jest 配置以支持更多模块转换
    添加了 `transform` 配置以使用 `babel-jest` 处理 JS 和 TS 文件，并调整了 `transformIgnorePatterns` 以包含 `@react-native` 模块。同时修正了 `moduleNameMapper` 中的正则表达式格式。

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,15 @@
 module.exports = {
   preset: 'jest-expo',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  },
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@react-native/.*))'
   ],
   setupFiles: ['<rootDir>/jest.setup.js'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
   moduleNameMapper: {
-    '\\.svg$': '<rootDir>/__mocks__/svgMock.js'
+    '\.svg$': '<rootDir>/__mocks__/svgMock.js'
   }
 }
